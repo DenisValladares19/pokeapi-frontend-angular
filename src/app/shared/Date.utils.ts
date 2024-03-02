f733d1970860c992e44a31e6a4f45c45367dda27
Denis Valladares <denisvalladares402@gmail.com>
2024-03-01 21:27:34 -0600

    add format date

---
+++ src/app/shared/Date.utils.ts
@@ -0,0 +1,11 @@
+export const MY_FORMATS = {
+  parse: {
+    dateInput: 'DD/MMM/YYYY',
+  },
+  display: {
+    dateInput: 'DD/MMM/YYYY',
+    monthYearLabel: 'DD MMMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'DDDD MMMM YYYY',
+  },
+};
