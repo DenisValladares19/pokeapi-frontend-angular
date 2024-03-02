6e01a0e9d8dfaa4b44de398843af274074372680
Denis Valladares <denisvalladares402@gmail.com>
2024-03-02 17:36:42 -0600

    integrate api load pokemons

--- src/app/pages/select-pokemons/select-pokemons.component.ts
+++ src/app/pages/select-pokemons/select-pokemons.component.ts
@@ -1,11 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { LoadingFullScreenComponent } from '../../shared/loading-full-screen/loading-full-screen.component';
+import { TitleComponent } from '../../shared/title/title.component';
+import { CardProfileComponent } from '../../components/card-profile/card-profile.component';
+import { PokemonListComponent } from '../../components/pokemon-list/pokemon-list.component';
+import { PokemonInit } from '../../interfaces/Pokemon.init';
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-select-pokemons',
   standalone: true,
-  imports: [LoadingFullScreenComponent],
+  imports: [
+    LoadingFullScreenComponent,
+    TitleComponent,
+    CardProfileComponent,
+    PokemonListComponent,
+  ],
   templateUrl: './select-pokemons.component.html',
   styleUrl: './select-pokemons.component.scss',
 })
-export class SelectPokemonsComponent {}
+export class SelectPokemonsComponent implements OnInit {
+  pokemons: PokemonInit[] = [];
+  loading: boolean = false;
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit(): void {
+    this.loading = true;
+    this.apiService
+      .fetch<PokemonInit[]>({
+        endpoint: '/pokemon',
+        params: { limit: '9' },
+      })
+      .subscribe({
+        next: (result) => {
+          this.pokemons = result.results;
+          setTimeout(() => {
+            this.loading = false;
+          }, 1000);
+        },
+        error: (error) => {
+          console.log('[error load pokemon]', error);
+          this.loading = false;
+        },
+      });
+  }
+}
