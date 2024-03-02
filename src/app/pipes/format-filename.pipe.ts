237d6ac0300b9477dacdbfe11cc95ce4291c2531
Denis Valladares <denisvalladares402@gmail.com>
2024-03-01 22:49:50 -0600

    add util pipe format filename

---
+++ src/app/pipes/format-filename.pipe.ts
@@ -0,0 +1,22 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'formatFilename',
+  standalone: true,
+})
+export class FormatFilenamePipe implements PipeTransform {
+  MAX_LEN = 20;
+  transform(value: string, ...args: unknown[]): string {
+    console.log(value);
+    if (!value) return '';
+    if (value.length <= this.MAX_LEN) return value;
+
+    const arrSplitted = value.split('.');
+    const extension = arrSplitted[arrSplitted.length - 1];
+    const stringJoined = arrSplitted
+      .filter((str) => str !== extension)
+      .join('.');
+
+    return `${stringJoined.slice(0, this.MAX_LEN)}... .${extension}`;
+  }
+}
