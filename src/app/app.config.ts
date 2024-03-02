640e73953fe6233ba0e7af78a54069916efb7b3b
Denis Valladares <denisvalladares402@gmail.com>
2024-03-02 09:28:45 -0600

    add ngrx dep global state management

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,11 +4,13 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideEnvironmentNgxMask } from 'ngx-mask';
+import { provideStore } from '@ngrx/store';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimationsAsync(),
     provideEnvironmentNgxMask(),
-  ],
+    provideStore()
+],
 };
