4dae3b54ab43690b75e7bb924de12e96d84c7286
Denis Valladares <denisvalladares402@gmail.com>
2024-03-03 19:33:14 -0600

    add progress bar component

---
+++ src/app/shared/progress-bar/progress-bar.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input } from '@angular/core';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+
+@Component({
+  selector: 'app-progress-bar',
+  standalone: true,
+  imports: [MatProgressBarModule],
+  templateUrl: './progress-bar.component.html',
+  styleUrl: './progress-bar.component.scss',
+})
+export class ProgressBarComponent {
+  @Input() color: string = 'green';
+  @Input() progress: number = 10;
+
+  constructor() {
+    document.documentElement.style.setProperty(
+      '--mdc-linear-progress-active-indicator-height',
+      '15px'
+    );
+    document.documentElement.style.setProperty(
+      '--mdc-linear-progress-track-height',
+      '15px'
+    );
+
+    document.documentElement.style.setProperty(
+      '--progress-bar-color-custom',
+      this.color
+    );
+  }
+}
