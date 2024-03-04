ad446d17d9ffeb48b5ad5b7067bd251d89618873
Denis Valladares <denisvalladares402@gmail.com>
2024-03-03 20:59:33 -0600

    calculate progress from base_stat and max stat

--- src/app/components/card-pokemon-home/card-pokemon-home.component.ts
+++ src/app/components/card-pokemon-home/card-pokemon-home.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { ProgressBarComponent } from '../../shared/progress-bar/progress-bar.component';
-import { Pokemon } from '../../interfaces/Pokemon';
+import { Pokemon, Stat } from '../../interfaces/Pokemon';
+import { maxStat } from '../../shared/Stats.utils';
 
 @Component({
   selector: 'app-card-pokemon-home',
@@ -11,4 +12,9 @@ import { Pokemon } from '../../interfaces/Pokemon';
 })
 export class CardPokemonHomeComponent {
   @Input() pokemon?: Pokemon;
+
+  getProgress(stat: Stat): number {
+    const maximum = maxStat[stat.name as keyof typeof maxStat];
+    return (stat.base_stat * 100) / maximum;
+  }
 }
