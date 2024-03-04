9f01951746457246a013ca0bc45b8a3c2442f3f2
Denis Valladares <denisvalladares402@gmail.com>
2024-03-03 20:58:54 -0600

    fix name max stat object

--- src/app/shared/Stats.utils.ts
+++ src/app/shared/Stats.utils.ts
@@ -2,7 +2,7 @@ export const maxStat = {
   PS: 255,
   Ataque: 190,
   Defensa: 230,
-  'Ataque especial': 194,
-  'Defensa especial': 230,
+  'Ataque Especial': 194,
+  'Defensa Especial': 230,
   Velocidad: 180,
 } as const;
