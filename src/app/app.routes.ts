c15cf14e9e975545b4dbb807bd65cab382772320
Denis Valladares <denisvalladares402@gmail.com>
2024-03-04 10:20:29 -0600

    add guard pokemons

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,14 @@ import { CreateProfileComponent } from './pages/create-profile/create-profile.co
 import { SelectPokemonsComponent } from './pages/select-pokemons/select-pokemons.component';
 import { HomeComponent } from './pages/home/home.component';
 import { guardHomeGuard } from './guards/guard-home.guard';
+import { guardPokemonsGuard } from './guards/guard-pokemons.guard';
 
 export const routes: Routes = [
   { path: '', component: CreateProfileComponent },
-  { path: 'pokemons', component: SelectPokemonsComponent },
+  {
+    path: 'pokemons',
+    component: SelectPokemonsComponent,
+    canActivate: [guardPokemonsGuard],
+  },
   { path: 'home', component: HomeComponent, canActivate: [guardHomeGuard] },
 ];
